1c0923deb27fc3bb5650c5f58535945a337ec031
Marat Kotik <mkotik97@gmail.com>
2022-11-06 14:58:49 -0500

    set up next app

---
+++ harhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x6bC5208cA5B12f9E22643Ef5DaDd8DE1A6D91eBC";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
